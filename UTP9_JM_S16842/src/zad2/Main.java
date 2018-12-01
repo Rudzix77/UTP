7b17539d6ad0531388f09a5cdf0d9355bc14a9cd
Michał <jankowski.michal@me.com>
2018-12-01 17:40:18 +0100

    Shitty SDKP

--- UTP9_JM_S16842/src/zad2/Main.java
+++ UTP9_JM_S16842/src/zad2/Main.java
@@ -7,12 +7,10 @@
 package zad2;
 
 
-import static zad2.Futil.processDir;
-
 public class Main {
   public static void main(String[] args) {
     String dirName = System.getProperty("user.home")+"/UTP6dir";
     String resultFileName = "UTP6res.txt";
-    processDir(dirName, resultFileName);
+    Futil.processDir(dirName, resultFileName);
   }
 }
